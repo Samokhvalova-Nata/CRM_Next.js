860d274cd3c1351f0da1fde0c53a970b320e1ff2
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-01-08 18:15:18 +0200

    Add clsx and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
+import ClientComponent from "./components/client-component";
+import ServerComponent from "./components/server-component";
+import ServerComponentCopy from "./components/server-component copy";
 import StatusLabel, {Status} from "./components/status-label";
+import { headers } from 'next/headers'; 
 
 export default function Home() {
+
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Active</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <StatusLabel disabled={true}>Disabled</StatusLabel>
+      <ServerComponent/>
+      <ClientComponent>
+        <ServerComponentCopy/>
+      </ClientComponent>
     </main>
   )
 };
