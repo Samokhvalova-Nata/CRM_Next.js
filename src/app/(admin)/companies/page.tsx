0f3767f23686dc7498f17fbb429083cc5f603d59
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-01-31 17:30:19 +0200

    Add Intercepting Routes for Companies Page

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,33 +1,22 @@
-import React from "react";
-import Header from "../../components/header";
-import Toolbar from "../../components/toolbar";
-import SearchInput from "../../components/search-input";
-import AddCompanyButton from "../../components/add-company-button";
-import CompanyTable from "../../components/company-table";
-import CompanyRow from "../../components/company-row";
-import { Status } from "../../components/status-label";
+import React from 'react';
+import CompanyTable from '@/app/components/company-table';
+import CompanyRow from '@/app/components/company-row';
+import { Status } from '@/app/components/status-label';
 
-export interface PageProps {};
+export interface PageProps {}
 
-export default function Page({ }: PageProps) {
-
-
-    return (
-        <div>
-            <Header>Companies</Header>
-            <Toolbar action={<AddCompanyButton/>}>
-                <SearchInput/>
-            </Toolbar>
-            <CompanyTable>
-                <CompanyRow 
-                    id={1} 
-                    category={"Products"} 
-                    company={"Costco"} 
-                    status={Status.Pending} 
-                    promotion={true} 
-                    country={"USA"} 
-                    joinedDate={"02.19.2023"}/>
-            </CompanyTable>
-        </div>
-    );
-};
+export default function Page({}: PageProps) {
+  return (
+    <CompanyTable>
+      <CompanyRow
+        id={1}
+        category="Products"
+        company="Costco"
+        status={Status.Pending}
+        promotion={true}
+        country="USA"
+        joinedDate="02.19.2023"
+      />
+    </CompanyTable>
+  );
+}
