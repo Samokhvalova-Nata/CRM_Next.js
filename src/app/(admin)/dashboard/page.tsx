26c6c9ef1f3e131dbfff072bf2f911a2d1c477e9
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-02-01 16:16:10 +0200

    Add Data fetching

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,18 +1,12 @@
-import MagicButton from "@/app/components/magic-button";
-import React from "react";
+import React from 'react';
+import Header from '@/app/components/header';
 
+export interface PageProps {}
 
-export interface PageProps {
-
-};
-
-export default function Page({ }: PageProps) {
-
-
-    return (
-        <main>
-            <h1 className="text-xl">Dashboard Page</h1>
-            <MagicButton/>
-        </main>
-    );
-};
+export default function Page({}: PageProps) {
+  return (
+    <>
+      <Header>Dashboard</Header>
+    </>
+  );
+}
